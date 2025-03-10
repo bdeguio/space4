8fb5a06422bc90ae6646ddfbeb26e18511786607
bdeguio <77205811+bdeguio@users.noreply.github.com>
2025-03-09 23:49:14 -0700

    Update gameLogic.js

--- gameLogic.js
+++ gameLogic.js
@@ -1,18 +1,52 @@
-import { path, properties } from "./board.js";
+let positionIndex = 0;
+let balance = 1500;
+let ownedProperties = [];
 
-export let positionIndex = 0;
-export let balance = 1500;
+function rollDice() {
+    const roll1 = Math.floor(Math.random() * 6) + 1;
+    const roll2 = Math.floor(Math.random() * 6) + 1;
+    const rollTotal = roll1 + roll2;
 
-export function rollDice() {
-    return Math.floor(Math.random() * 6) + 1 + Math.floor(Math.random() * 6) + 1;
-}
+    diceResult.textContent = `🎲 ${roll1} + 🎲 ${roll2} = ${rollTotal}`;
 
-export function movePlayer(rollTotal) {
     let previousIndex = positionIndex;
     positionIndex = (positionIndex + rollTotal) % path.length;
 
     if (positionIndex < previousIndex) {
         balance += 200;
-        console.log("Passed GO! +$200 added.");
     }
+
+    updatePlayerUI();
+}
+
+function buyProperty() {
+    let currentProperty = properties[positionIndex];
+
+    if (!currentProperty.owned && currentProperty.price > 0 && balance >= currentProperty.price) {
+        balance -= currentProperty.price;
+        currentProperty.owned = true;
+        ownedProperties.push(currentProperty);
+        updatePlayerUI();
+    }
+}
+
+function upgradeProperty(property) {
+    if (balance >= property.houseCost) {
+        balance -= property.houseCost;
+        property.houses += 1;
+        updatePlayerUI();
+    } else {
+        alert("Not enough money to upgrade!");
+    }
+}
+
+function downgradeProperty(property) {
+    if (property.houses > 0) {
+        property.houses -= 1;
+        balance += property.houseCost / 2;
+    } else if (property.houses === 0 && !property.mortgaged) {
+        property.mortgaged = true;
+        balance += property.mortgageValue;
+    }
+    updatePlayerUI();
 }
