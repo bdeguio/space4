ca78a5f239e9ebb93f571ee307df2d1a128036f8
bdeguio <77205811+bdeguio@users.noreply.github.com>
2025-03-09 23:24:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const positionDisplay = document.getElementById("position");
     const balanceDisplay = document.getElementById("balance");
     const propertiesDisplay = document.getElementById("properties");
-    const buyButton = document.getElementById("buyButton");
 
     createBoard(board);
     const cells = document.querySelectorAll(".cell");
-    updateUI(cells, positionDisplay, balanceDisplay, propertiesDisplay, buyButton);
+    updateUI(cells, positionDisplay, balanceDisplay, propertiesDisplay);
 
     rollButton.addEventListener("click", () => {
-        diceResult.textContent = `You rolled a ${rollDice()}`;
-        movePlayer(rollDice());
-        updateUI(cells, positionDisplay, balanceDisplay, propertiesDisplay, buyButton);
+        const rollTotal = rollDice();
+        diceResult.textContent = `You rolled a ${rollTotal}`;
+        movePlayer(rollTotal);
+        updateUI(cells, positionDisplay, balanceDisplay, propertiesDisplay);
     });
 });
