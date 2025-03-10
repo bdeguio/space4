14f8f75e2cab394f554c7c958967245a99fdebb5
bdeguio <77205811+bdeguio@users.noreply.github.com>
2025-03-09 23:20:06 -0700

    Update gameLogic.js

--- gameLogic.js
+++ gameLogic.js
@@ -2,7 +2,6 @@ import { path, properties } from "./board.js";
 
 export let positionIndex = 0;
 export let balance = 1500;
-export let ownedProperties = [];
 
 export function rollDice() {
     return Math.floor(Math.random() * 6) + 1 + Math.floor(Math.random() * 6) + 1;
@@ -16,22 +15,4 @@ export function movePlayer(rollTotal) {
         balance += 200;
         console.log("Passed GO! +$200 added.");
     }
-}
-
-export function canBuyProperty() {
-    let currentProperty = properties[positionIndex];
-    return currentProperty.price > 0 && !currentProperty.owner && balance >= currentProperty.price;
-}
-
-export function buyProperty() {
-    let currentProperty = properties[positionIndex];
-
-    if (canBuyProperty()) {
-        balance -= currentProperty.price;
-        currentProperty.owner = "Player";
-        ownedProperties.push(currentProperty.name);
-        console.log(`Purchased ${currentProperty.name} for $${currentProperty.price}.`);
-        return true;
-    }
-    return false;
 }
